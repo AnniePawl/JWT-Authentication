25a3dd99fe4868f868e880cf2d2b86ea1f0e6fe6
Anna Pawl <annampawl@gmail.com>
2019-06-18 11:05:56 -0700

    built first route

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+
+
+// Routes 
+app.get('/', (req, res) => {
+  res.send('JWT is Badass')
+});
+
+// Server Start
+app.listen(3000, () => {
+  console.log('JTW Authentication Almost in Action')
+});
